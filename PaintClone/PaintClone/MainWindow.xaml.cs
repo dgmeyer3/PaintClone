72dae17a90ce140fa6c83c3359ef2e803a83b95f
Donny Meyer <dgm74@miami.edu>
2023-01-23 17:15:51 -0500

    drawing/changing colors/changing size now enabled

--- PaintClone/PaintClone/MainWindow.xaml.cs
+++ PaintClone/PaintClone/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +21,8 @@ namespace PaintClone
     /// </summary>
     public partial class MainWindow : Window
     {
+        double brushDouble = 3;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,9 +33,54 @@ namespace PaintClone
 
         }
 
-        private void Color_Click(object sender, RoutedEventArgs e)
+        private void Circle_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Triangle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Upsize_Click(object sender, RoutedEventArgs e)
+        {
+            //brushSize textbox needs a method that updates the brushAttr height/width when manually changed outside of a button cick
+            brushDouble++;
+
+            brushSize.Text = brushDouble.ToString();
+            
+            strokeAttr.Height = brushDouble;
+            strokeAttr.Width = brushDouble;
+        }
+
+        private void Downsize_Click(object sender, RoutedEventArgs e)
+        {
+            brushDouble--;
+
+            brushSize.Text = brushDouble.ToString();
+
+            strokeAttr.Height = brushDouble;
+            strokeAttr.Width = brushDouble;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+        }
+
+        private void Rectangle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void colorBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string comboitem = colorBox.SelectedItem.ToString();
+            var brushColor = comboitem.Split(' '); 
+
+            strokeAttr.Color = (Color)ColorConverter.ConvertFromString(brushColor[1]);
         }
     }
 }
